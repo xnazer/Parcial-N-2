362fb5a7e2890eb1aa423d170da58ed495e9389f
Jerogp <155851299+Jerogp@users.noreply.github.com>
2025-03-21 17:26:55 -0500

    main.cpp

--- carpeta juego de cartas/main.cpp
+++ carpeta juego de cartas/main.cpp
@@ -1,1 +1,16 @@
+int main() {
+    Carta unaCarta(2, 2);
+    unaCarta.mostrar();
+    cout << endl;
 
+    Mazo baraja;
+    cout << "Mazo original: " << endl;
+    baraja.mostrar();
+    cout << endl;
+
+    baraja.barajar();  // Barajar el mazo
+    cout << "Mazo barajado: " << endl;
+    baraja.mostrar();
+
+    return 0;
+}
