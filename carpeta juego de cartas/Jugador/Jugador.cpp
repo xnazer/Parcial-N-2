46d5bcc1d5f660cf714dd95ffd6d6b50317d854d
Jerogp <155851299+Jerogp@users.noreply.github.com>
2025-03-27 17:02:32 -0500

    Jugador.cpp

---
+++ carpeta juego de cartas/Jugador/Jugador.cpp
@@ -0,0 +1,30 @@
+#include "Jugador.h"
+#include <iostream>
+
+using namespace std;
+
+Jugador::Jugador(int i, int cantidadCartas, vector<Carta> &mazo) {
+    this->id = i;
+    this->mano = Mano(cantidadCartas, mazo);
+}
+
+void Jugador::mostrarMano() {
+    cout << "Jugador " << id << " - Mano: ";
+    mano.mostrar();
+}
+
+void Jugador::almacenarCarta(Carta c) {
+    boveda.push_back(c);
+}
+
+int Jugador::contarCartasBoveda() {
+    return boveda.size();
+}
+
+void Jugador::mostrarBoveda() {
+    cout << "Bodega del Jugador " << id << " (" << contarCartasBoveda() << " cartas): ";
+    for (Carta c : boveda) {
+        c.mostrar();
+    }
+    cout << endl;
+}
